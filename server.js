cac276f41351eed2ba746ac523c0e0d795cb75ff
Pro602 <Weera54440@gmail.com>
2026-04-28 13:10:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/scan", (req, res) => {
+  console.log("📥 Data from device:", req.body);  // 🔥 สำคัญมาก
+
+  res.json({ status: "ok" });
+});
+
+app.get("/", (req, res) => {
+  res.send("Scan API is running");
+});
+
+app.listen(3000, () => {
+  console.log("🚀 Server started");
+});
